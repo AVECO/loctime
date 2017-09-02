fbc3cd1809512f3084c047c29a65884225197061
Arvin San Andres <arvinsanandres@aveco.com.ph>
2017-09-03 02:43:02 +0800

    loctime

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 
 func main() {
 
-	t := "2017-08-31 14:15:00"
-	tp, _ := time.Parse("2006-01-02 15:04:05", t)
+	tLoc, _ := time.LoadLocation("Asia/Manila")
+
+	t := "2017-09-03 02:00:00"
+	tp, _ := time.ParseInLocation("2006-01-02 15:04:05", t, tLoc)
 	fmt.Println(tp)
 
-	t2, _ := time.LoadLocation("Asia/Manila")
-	t3 := time.Now().In(t2)
+	t3 := time.Now().In(tLoc)
 	fmt.Println("Location: ", t3.Location(), " Time: ", t3)
 
 	td := t3.Sub(tp)
